1aa03dcb0150c6df913326ef028145de8855ca1b
Aniruddh Agarwal <aaniruddh99@gmail.com>
2016-04-07 14:16:39 +0800

    Check for HTTP errors when fetching API

--- src/client.js
+++ src/client.js
@@ -12,14 +12,28 @@ import App from './components/App';
 Promise.polyfill();
 injectTapEventPlugin();
 
+const checkStatus = response => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+
+  const error = new Error(response.statusText);
+  error.response = response;
+  throw error;
+};
+
+const parseJSON = response => response.json();
+
 // Render app once both the APIs have responded
 fetch('http://fcctop100.herokuapp.com/api/fccusers/top/recent')
-  .then(response => response.json())
+  .then(checkStatus)
+  .then(parseJSON)
   .then(topRecent => {
     fetch('http://fcctop100.herokuapp.com/api/fccusers/top/alltime')
-      .then(response => response.json())
+      .then(checkStatus)
+      .then(parseJSON)
       .then(topAllTime => {
-        render(<App topAllTime={topAllTime} topRecent={topRecent} />,
+        render(<App alltime={topAllTime} recent={topRecent} />,
                 document.getElementById('app'));
       });
   });
