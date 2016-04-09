d4f8cfc05b1e34b1c3107472405fe994656cb190
Aniruddh Agarwal <aaniruddh99@gmail.com>
2016-04-09 12:43:54 +0800

    Add loading screen

---
+++ src/components/LoadingScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoadingBar = () => (
+  <div className="chart">
+    <div className="one" />
+    <div className="two" />
+    <div className="three" />
+    <div className="four" />
+  </div>
+);
+
+export default LoadingBar;
